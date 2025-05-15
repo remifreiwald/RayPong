de334b4ecc20ec358bc01b855d3e36c707fbfac6
remifreiwald <remifreiwald@gmail.com>
2025-05-16 00:39:28 +0200

    Added classes for Player and CPU Draw paddles for both

--- RayPong/main.cpp
+++ RayPong/main.cpp
@@ -8,14 +8,49 @@
 
 #include "raylib.h"
 
+int windowWidth = 1280;
+int windowHeight = 720;
+
+class Player {
+public:
+	Vector2 pos;
+	float width;
+	float height;
+	Color color;
+
+	void Draw() {
+		DrawRectangle(pos.x - width / 2.f, pos.y - height / 2.f, width, height, color);
+	}
+};
+
+class CPU : public Player {
+
+};
+
 int main() {
-	InitWindow(1280, 720, "RayPong");
+	InitWindow(windowWidth, windowHeight, "RayPong");
 	SetTargetFPS(60);
+
+	Player player = Player();
+	player.pos = Vector2{ (float)windowWidth - 50, windowHeight / 2.f };
+	player.width = 25;
+	player.height = 120;
+	player.color = WHITE;
+
+	CPU cpu = CPU();
+	cpu.pos = Vector2{ (float)50, windowHeight / 2.f };
+	cpu.width = 25;
+	cpu.height = 120;
+	cpu.color = WHITE;
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 
 		ClearBackground(BLACK);
+		DrawLine(windowWidth / 2.f, 0, windowWidth / 2.f, windowHeight, WHITE);
+
+		player.Draw();
+		cpu.Draw();
 
 		EndDrawing();
 	}
