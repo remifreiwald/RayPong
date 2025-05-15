66cbd21e43650a56f079e03117cd38a99ef3b559
remifreiwald <remifreiwald@gmail.com>
2025-05-16 00:03:28 +0200

    Projektdateien hinzufügen.

---
+++ RayPong/main.cpp
@@ -0,0 +1,13 @@
+/*
+* Copyright (c) 2025, Remi Freiwald
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+#include "raylib.h"
+
+int main() {
+	return 0;
+}
